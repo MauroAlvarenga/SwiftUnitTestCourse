977d132be069aced3e290a825a96a93125992d63
Mauro Emmanuel Alvarenga <mauro.alvarenga@mercadolibre.com>
2023-02-28 16:48:44 -0300

    Add more tests for practice

---
+++ TestCourse/Print.swift
@@ -0,0 +1,8 @@
+//
+//  Print.swift
+//  TestCourse
+//
+//  Created by Mauro Emmanuel Alvarenga on 28/02/2023.
+//
+
+import Foundation
---
+++ TestCourseTests/Mock/AddTest.swift
@@ -0,0 +1,8 @@
+//
+//  AddTest.swift
+//  TestCourseTests
+//
+//  Created by Mauro Emmanuel Alvarenga on 13/12/2022.
+//
+
+import Foundation
